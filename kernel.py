2a1e7651832d58d572d1ee8e30d5ee3d95ff9831
Franco <franco.tapia.c@usach.cl>
2020-06-16 22:20:27 -0400

    Transformada Fourier

--- kernel.py
+++ kernel.py
@@ -2,20 +2,24 @@ import numpy as np
 
 def crearKernelGaussiano():
     # Gaussian kernel
-    kernel = [[1 / 256, 4  / 256,  6 / 256,  4 / 256, 1 / 256],
+    kernel = np.array(
+              [[1 / 256, 4  / 256,  6 / 256,  4 / 256, 1 / 256],
               [4 / 256, 16 / 256, 24 / 256, 16 / 256, 4 / 256],
               [6 / 256, 24 / 256, 36 / 256, 24 / 256, 6 / 256],
               [4 / 256, 16 / 256, 24 / 256, 16 / 256, 4 / 256],
               [1 / 256, 4  / 256,  6 / 256,  4 / 256, 1 / 256]]
+              )
     
     return kernel
 
 def crearKernelBordes():
     #kernel detector de bordes
-    kernel = [[1, 2, 0, -2, -1],
+    kernel = np.array(
+              [[1, 2, 0, -2, -1],
               [1, 2, 0, -2, -1],
               [1, 2, 0, -2, -1],
               [1, 2, 0, -2, -1],
               [1, 2, 0, -2, -1]]
+              )
     
     return kernel
--- main.py
+++ main.py
@@ -3,15 +3,30 @@ import matplotlib.pyplot as plt
 import lecturaEscritura as lecturaEscritura
 import kernel as kernel
 import convolucion as convolucion
+import fourier as fourier
 
 nombre = "lena512.bmp"
 imagen = lecturaEscritura.leerImagen(nombre)
-kernel = kernel.crearKernel1()
-convolucion = convolucion.aplicarConvolucion(imagen,kernel)
 
-print(convolucion)
-lecturaEscritura.escribirImagen(convolucion)
+#Suavizado Gaussiano
+kernelGaussiano = kernel.crearKernelGaussiano()
+convolucionGaussiano = convolucion.aplicarConvolucion(imagen,kernelGaussiano)
 
+#Detector de bordes
+kernelBordes = kernel.crearKernelBordes()
+convolucionBorde = convolucion.aplicarConvolucion(imagen,kernelBordes)
+
+#Transformada de fourier
+fourier = fourier.transformadaFourier(imagen)
+fourierGaussiano = fourier.transformadaFourier(convolucionGaussiano)
+fourierBorde = fourier.transformadaFourier(convolucionBorde)
+
+#Guardando resultados
+lecturaEscritura.escribirImagen(convolucionGaussiano)
+lecturaEscritura.escribirImagen(convolucionBorde)
+lecturaEscritura.escribirImagen(fourier)
+lecturaEscritura.escribirImagen(fourierGaussiano)
+lecturaEscritura.escribirImagen(fourierGausfourierBordesiano)
 
 
   
